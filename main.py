1adae79589ddd481fb2a2648c305e5ea7793bed5
Noah Snider <nsnider0@gmail.com>
2021-09-18 20:35:21 -0400

    update entry point

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import requests
 from google.oauth2 import service_account
 import pandas as pd
 from datetime import datetime
+import logging
+from string import Template
+import config
+from google.cloud import bigquery
 
 
 # define bigquery project and dataset name
@@ -31,13 +35,32 @@ player_stats['loaded_at'] = loaded_at
 player_types = pd.DataFrame(data['element_types'])
 player_types['loaded_at'] = loaded_at
 
-def main():
-    # load to bigquery tables
-    gameweeks.to_gbq('raw.gameweeks', project_id=bq_project, if_exists='replace', credentials=credentials)
-    teams.to_gbq('raw.teams', project_id=bq_project, if_exists='replace', credentials=credentials)
-    players.to_gbq('raw.players', project_id=bq_project, if_exists='replace', credentials=credentials)
-    player_stats.to_gbq('raw.player_stats', project_id=bq_project, if_exists='replace', credentials=credentials)
-    player_types.to_gbq('raw.player_types', project_id=bq_project, if_exists='replace', credentials=credentials)
+def main(data, context):
+    """Triggered from a message on a Cloud Pub/Sub topic.
+    Args:
+        data (dict): Event payload.
+        context (google.cloud.functions.Context): Metadata for the event.
+    """
+    try:
+        current_time = datetime.utcnow()
+        log_message = Template('Cloud Function was triggered on $time')
+        logging.info(log_message.safe_substitute(time=current_time))
+
+        try:
+            gameweeks.to_gbq('raw.gameweeks', project_id=bq_project, if_exists='replace', credentials=credentials)
+            teams.to_gbq('raw.teams', project_id=bq_project, if_exists='replace', credentials=credentials)
+            players.to_gbq('raw.players', project_id=bq_project, if_exists='replace', credentials=credentials)
+            player_stats.to_gbq('raw.player_stats', project_id=bq_project, if_exists='replace', credentials=credentials)
+            player_types.to_gbq('raw.player_types', project_id=bq_project, if_exists='replace', credentials=credentials)
+
+        except Exception as error:
+            log_message = Template('Query failed due to '
+                                   '$message.')
+            logging.error(log_message.safe_substitute(message=error))
+
+    except Exception as error:
+        log_message = Template('$error').substitute(error=error)
+        logging.error(log_message)
 
 if __name__ == "__main__":
-   main()
+   main('data','context')
